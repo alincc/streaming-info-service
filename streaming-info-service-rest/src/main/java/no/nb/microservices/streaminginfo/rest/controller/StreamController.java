5903307e5b94f8beb7dda9afb86f04e95e55955f
Andreas Bjørnådal <bjornadal@gmail.com>
2015-10-27 14:48:29 +0100

    Requestmapping

--- streaming-info-service-rest/src/main/java/no/nb/microservices/streaminginfo/rest/controller/StreamController.java
+++ streaming-info-service-rest/src/main/java/no/nb/microservices/streaminginfo/rest/controller/StreamController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/streaming")
 public class StreamController {
 
     private static final Logger LOG = LoggerFactory.getLogger(StreamController.class);
